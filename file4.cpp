12029feba055cec983078576e739c1a2e48bd91a
BhuvanSharma02 <121033984+BhuvanSharma02@users.noreply.github.com>
2024-08-01 18:20:54 +0530

    Add files via upload

---
+++ file4.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#include<fstream>
+
+using namespace std;
+
+int main () {
+    string user, pass;
+    cout << "Enter Username: ";
+    cin >> user;
+    cout << "Enter Password: ";
+    cin >> pass;
+    ofstream file("Pass.txt");
+    if(!file) {
+        cout << "Error!";
+        return 1;
+    }
+    file << "Username: " << user << endl << "Password: " << pass;
+    cout << "\n\nUser Info Stored Successfully!!";
+    file.close();
+}
